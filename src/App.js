316c85ac1accbecf2f1ebf44ef57090e28d7aece
”0xAureoleNode“ <aureolenode@gmail.com>
2022-10-02 12:16:41 +0800

    add component instantiation & refactoring some func components

--- src/App.js
+++ src/App.js
@@ -18,28 +18,36 @@ const list = [
     objectID: 1,
   },
 ];
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <Search />
+    <hr />
 
-      <hr />
+    <List />
+  </div>
+);
 
-      <ul>
-        {list.map((item) => (
-          <li key={item.objectID}>
-            <span>
-              <a href="{item.url}">{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href="{item.url}">{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
+
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input type="text" id="search" />
+  </div>
+);
+
 export default App;
