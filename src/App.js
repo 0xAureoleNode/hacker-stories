480156ea650cdbf97d84e8f6197b5161bab19eea
”0xAureoleNode“ <aureolenode@gmail.com>
2022-10-02 14:17:16 +0800

    event handler

--- src/App.js
+++ src/App.js
@@ -43,11 +43,16 @@ const List = () => (
   </ul>
 );
 
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input type="text" id="search" />
-  </div>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" onChange={handleChange} />
+    </div>
+  );
+};
 
 export default App;
